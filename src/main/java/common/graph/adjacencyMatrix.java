9c9a544d07e96524b9725c2b2e51251268ec1e14
Alexandre Decuq <alexandre.decuq@murex.com>
2018-03-14 19:43:59 +0100

    Initial commit

---
+++ src/main/java/common/graph/adjacencyMatrix.java
@@ -0,0 +1,8 @@
+package common.graph;
+
+public class adjacencyMatrix {
+
+   public static void test() {
+      System.out.println("Hello world!");
+   }
+}
---
+++ src/test/java/common/graph/adjacencyMatrixTest.java
@@ -0,0 +1,14 @@
+package common.graph;
+
+import static common.graph.adjacencyMatrix.test;
+
+import org.junit.Test;
+
+public class adjacencyMatrixTest {
+
+   @Test
+   public void
+   it_should() {
+      test();
+   }
+}
